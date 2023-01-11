94693510db4ac7c178a83083b5ca227cc9728777
Lennox Charles <lennyaiko17@gmail.com>
2023-01-12 00:03:58 +0100

    apollo setup completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/client'
+
+// components
+import BookList from './components/BookList'
+
+// apollo client setup
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+})
 
 function App() {
   return (
-    <div id="main">
-      <h1>Ninja's Reading List</h1>
-    </div>
+    <ApolloProvider client={client}>
+      <div id="main">
+        <h1>Ninja's Reading List</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 }
 
